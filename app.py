383e1b35baa3656f8ef1b2bb253d6601ed4a675f
izacistan <45190655+Izacistan@users.noreply.github.com>
2024-11-22 14:16:26 -0600

    app file updates, created db
    1. Added GET and POST methods to index function in app.py.
    2. Created database model and created simple db.
    3. Added tables to index.html. Will remove later, just want some basic structure that I don't need to style too hard.
    4. Added basic table styles.
    5. Updated base.html CSS file paths to proper Flask syntax.
    6. Added the __pycache__ file to gitignore.

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+# Initialize the `rr_tasks` Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rr_tasks.db'
+tasks_db = SQLAlchemy(app)
+
+
+class Todo(tasks_db.Model):
+    id = tasks_db.Column(tasks_db.Integer, primary_key=True)
+    content = tasks_db.Column(tasks_db.String(200), nullable=False)
+    completed = tasks_db.Column(tasks_db.Integer, default=0)
+    date_created = tasks_db.Column(tasks_db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
 # Default route, so we do not get 404 error
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return 'Testing post method...'
+    else:
+        return render_template('index.html')
+
 
 if __name__ == "__main__":
     # Run the app, with debug on, so we can see errors on screen.
