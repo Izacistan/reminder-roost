ac4816ddd379d470cc73861ac95ed59de5536325
izacistan <45190655+Izacistan@users.noreply.github.com>
2024-11-22 10:18:49 -0600

    Base styles, folder additions
    1. Added some base styles for typography, color variables, primary button, and page center. More to come.
    2. app.py is now set to render the index.html template.
    3. Added some component files for header and footer, but haven't set them up properly yet. Placeholder at the moment.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Default route, so we do not get 404 error
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     # Run the app, with debug on, so we can see errors on screen.
