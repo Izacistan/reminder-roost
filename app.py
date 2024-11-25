3997b6004f763c896f23c8311b024178525cb627
isaachillaker <127630777+isaachillaker@users.noreply.github.com>
2024-11-25 14:02:17 -0600

    db auto creation, update and delete tasks
    1. Added behavior to auto-create a db when the app is run. This is for testing purposes and will be fleshed out further, down the road.
    2. Added basic delete task function to remove tasks from the db.
    3. Added basic update task function to update text content of task in db.
    4. Added update.html for updating the task on the front-end.

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
 # Initialize the `rr_tasks` Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rr_tasks.db'
-tasks_db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
 
 
-class Todo(tasks_db.Model):
-    id = tasks_db.Column(tasks_db.Integer, primary_key=True)
-    content = tasks_db.Column(tasks_db.String(200), nullable=False)
-    completed = tasks_db.Column(tasks_db.Integer, default=0)
-    date_created = tasks_db.Column(tasks_db.DateTime, default=datetime.utcnow)
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -23,11 +23,50 @@ class Todo(tasks_db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        return 'Testing post method...'
-    else:
-        return render_template('index.html')
+        task_content = request.form['content']  # Grabbing data from <input> where name is 'content'
+        new_task = Todo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')  # redirect to index.html
+        except:
+            return 'There was an issue adding your task.'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+# Delete task functionality
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task.'
+
+# Update task functionality
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task.'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
+    # Set up the application context and create the database tables
+    with app.app_context():
+        db.create_all()
+
     # Run the app, with debug on, so we can see errors on screen.
     app.run(debug=True)
